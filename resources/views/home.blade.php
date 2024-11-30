be51de94b8ba3cd6ca3c001cc3deb0a217c5466c
Charalambos Tetrimidas <maverick.st@gmail.com>
2024-11-30 13:00:17 +0200

    Add tailwind css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,18 @@
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel....</p>
+    <p class="p-10 bg-red-100">Welcome to this beautiful admin panel....</p>
 @stop
 
 @section('css')
     {{-- Add here extra stylesheets --}}
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @stop
+
+
+@push('css')
+<link href="{{ mix('resources/css/app.css') }}" rel="stylesheet">
+@endpush
 
 @section('js')
     <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
