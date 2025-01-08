c27fadd696c295b8ca367a9e826524d81ef5bc51
Charalambos Tetrimidas <maverick.st@gmail.com>
2025-01-08 22:55:54 +0200

    Create admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,9 +15,17 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@netwise.gr',
+            'password' => bcrypt('netwise'),
         ]);
+
+        $user->assignRole('Admin');
     }
 }
