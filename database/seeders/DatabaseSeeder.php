e0154c75c6dd8bd6dee873ee0891ba4d9013b2ff
Charalambos Tetrimidas <maverick.st@gmail.com>
2025-01-15 15:22:10 +0200

    change database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\CustomerCategory;
+use App\Models\Source;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,15 +17,27 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
+        //Roles
         $role = Role::create(['name' => 'Admin']);
         $role = Role::create(['name' => 'web']);
 
+        //User
         $user = User::create([
             'name' => 'admin',
             'email' => 'admin@netwise.gr',
             'password' => bcrypt('netwise'),
         ]);
-
         $user->assignRole('Admin');
+
+
+        //Customer Category
+        CustomerCategory::create(['name' => 'A']);
+        CustomerCategory::create(['name' => 'B']);
+        CustomerCategory::create(['name' => 'C']);
+
+        //Customer Category
+        Source::create(['name' => 'Internet']);
+        Source::create(['name' => 'Σύσταση']);
+        Source::create(['name' => 'Διαφήμιση']);
     }
 }
